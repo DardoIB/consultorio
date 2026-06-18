88d83c223cef71dc637a009616f8fe9ca1127d05
DardoIB <audioguias.salta@gmail.com>
2026-06-18 09:52:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,9 @@ from datetime import date
 
 from supabase import create_client
 import streamlit as st
+
 st.write("Secrets disponibles:")
 st.write(st.secrets)
-#url = st.secrets["SUPABASE_URL"]
-#key = st.secrets["SUPABASE_KEY"]
 
 supabase = create_client(url, key)
 
